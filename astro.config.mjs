f6f8195031de6fd65c49c6f3583fce1351df7556
tellebma <maxime.bellet@orange.com>
2025-04-17 19:40:54 +0200

    fix: set correct base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://tellebma.github.io',
-  base: 'cv',
-})
+  base: '/cv/',
+});
