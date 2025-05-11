a6cd1d6dc8c14f0f1c835e5807bd3e73f81af9a6
Maxime BELLET <66943979+tellebma@users.noreply.github.com>
2025-05-11 18:53:43 +0200

    revert: base url config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: '/',
+  base: '/cv',
   vite: {
     plugins: [tailwindcss()],
   },
